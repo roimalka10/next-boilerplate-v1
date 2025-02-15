ceaad45fe9e6ce00a0bacd69547d2b023f7a689e
roimalka10 <roimalka10@gmail.com>
2025-02-15 14:01:17 +0200

    changed caching method for api

--- src/_lib/api.js
+++ src/_lib/api.js
@@ -1,19 +1,17 @@
-import { cache } from "react";
-
-export const fetchPages = cache(async () => {
+export const fetchPages = async () => {
   try {
-    const response = await fetch(`http://localhost:5000/pages`);
+    const response = await fetch(`http://localhost:5000/pages`, { cache: "force-cache" });
     const pages = await response.json();
     return pages;
   } catch (error) {
     console.error("Error fetching pages:", error);
     return null;
   }
-});
+};
 
-export const fetchPageBySlug = cache(async (slug) => {
+export const fetchPageBySlug = async (slug) => {
   try {
-    const response = await fetch("http://localhost:5000/pages");
+    const response = await fetch(`http://localhost:5000/pages`, { cache: "force-cache" });
     const page = await response.json();
 
     if (!slug || slug === "") {
@@ -25,15 +23,17 @@ export const fetchPageBySlug = cache(async (slug) => {
     console.error("Error fetching page:", error);
     return null;
   }
-});
+};
 
-export const fetchPageData = cache(async (categoryId) => {
+export const fetchPageData = async (categoryId) => {
   try {
-    const response = await fetch(`http://localhost:5000/pages/${categoryId}`);
+    const response = await fetch(`http://localhost:5000/pages/${categoryId}`, {
+      cache: "force-cache",
+    });
     const pagesData = await response.json();
     return pagesData;
   } catch (error) {
     console.error("Error fetching page content:", error);
     return null;
   }
-});
+};
